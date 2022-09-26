96351a425f046d5c537b61a7c1be8d1c57c20e3b
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-26 13:03:31 -0400

    connects to mongoose

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const URL = process.env.MONGODB_URL;
 const express = require('express');
 const app = express();
 
+// import mongoose
+const mongoose = require("mongoose");
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+// Connection Events
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
 app.get("/", (req, res)=>{
     res.send("hello sir");
 });
