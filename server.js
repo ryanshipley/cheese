267737e0633ea702d3c0679edd9ccc4750bcafc9
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-26 13:15:25 -0400

    adds home and create

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const app = express();
 // import mongoose
 const mongoose = require("mongoose");
 
+const cors = require("cors");
+const morgan = require("morgan");
+
+
+const CheeseSchema = new mongoose.Schema({
+    name : String,
+    countryOfOrigin : String,
+    image : String
+});
+
+const Cheese = mongoose.model("Cheese", CheeseSchema);
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
 ///////////////////////////////
 // DATABASE CONNECTION
 ////////////////////////////////
@@ -24,6 +40,22 @@ mongoose.connection
 
 app.get("/", (req, res)=>{
     res.send("hello sir");
+});
+
+app.get("/cheese", async(req, res) =>{
+    try{
+        res.json(await Cheese.find({}));
+    } catch (error){
+        res.status(400).json(error);
+    };
+});
+
+app.post("/cheese", async (req, res)=>{
+    try{
+        res.json(await Cheese.create(req.body));
+    } catch (error){
+        res.status(400).json(error);
+    };
 });
 
 app.listen(PORT);
